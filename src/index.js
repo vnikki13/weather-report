3823443cd1c4e3bea25cd3d522692a8335a551bd
Weixi He <heweixiguge@gmai.com>
2024-12-03 11:04:20 -0500

    finish wave 2 landscape by weixi

--- src/index.js
+++ src/index.js
@@ -4,23 +4,28 @@ const changeColor = (temperature) =>{
     const element = document.getElementById("temperatureSection");
     if(temperature >= 80){
         element.classList.forEach((className) => COLORS.includes(className) ? element.classList.remove(className) : className )
-        element.classList.add('red')
+        element.classList.add('red');
+        landscape.textContent = '🌵__🐍_🦂_🌵🌵__🐍_🏜_🦂';
     }
     else if(temperature >= 70 && temperature <=79){
         element.classList.forEach((className) => COLORS.includes(className) ? element.classList.remove(className) : className )
-        element.classList.add('orange')
+        element.classList.add('orange');
+        landscape.textContent = '🌸🌿🌼__🌷🌻🌿_☘️🌱_🌻🌷';
     }
     else if(temperature >= 60 && temperature <=69){
         element.classList.forEach((className) => COLORS.includes(className) ? element.classList.remove(className) : className )
-        element.classList.add('yellow')
+        element.classList.add('yellow');
+        landscape.textContent = '🌾🌾_🍃_🪨__🛤_🌾🌾🌾_🍃';
     }
     else if(temperature >= 50 && temperature <=59){
         element.classList.forEach((className) => COLORS.includes(className) ? element.classList.remove(className) : className )
-        element.classList.add('green')
+        element.classList.add('green');
+        landscape.textContent = '🌲🌲⛄️🌲⛄️🍂🌲🍁🌲🌲⛄️🍂🌲';
     }
     else {
         element.classList.forEach((className) => COLORS.includes(className) ? element.classList.remove(className) : className )
-        element.classList.add('teal')
+        element.classList.add('teal');
+        landscape.textContent = '🌲🌲⛄️🌲⛄️🍂🌲🍁🌲🌲⛄️🍂🌲';
     }
 }
 
@@ -37,12 +42,13 @@ const decreaseTemp = () => {
     const element = document.getElementById("tempValue");
     value = parseInt(element.textContent) || 0;
     changeColor(value)
-    element.innerText = (value + 1);
+    element.innerText = (value - 1);
 }
 
 
 const increment = document.getElementById("increaseTempControl");
 const decrement = document.getElementById("decreaseTempControl");
+const landscape = document.getElementById('landscape');
 
 increment.addEventListener("click", increaseTemp);
 decrement.addEventListener("click",decreaseTemp);
