d30fff1f780ec2a3a5aeb966d7e4e598fa4504a2
Weixi He <heweixiguge@gmai.com>
2024-12-03 21:21:48 -0500

    fix by myself weixi

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
-// Initialize temperature
-let temperature = 50;
+// Global variable
+let temperature = 65;
 
 // Get DOM elements
 const tempValue = document.getElementById('tempValue');
 const increaseTempControl = document.getElementById('increaseTempControl');
 const decreaseTempControl = document.getElementById('decreaseTempControl');
 const landscape = document.getElementById('landscape');
+const currentTempButton = document.getElementById("currentTempButton");
 
 // Update temperature and related effects
 const updateTemperature = () => {
@@ -29,17 +30,58 @@ const updateTemperature = () => {
     landscape.textContent = '🌲🌲⛄️🌲⛄️🍂🌲🍁🌲🌲⛄️🍂🌲';
   }
 };
+// founction
+
+const increaseTemperature = () => {
+    temperature += 1;
+    updateTemperature();
+}
+
+const decreaseTemperature = () => {
+    temperature -= 1;
+    updateTemperature();
+}
+
+const getTemp = () => {
+    axios
+    .get('http://127.0.0.1:5000/location', {params: {q: 'seattle'}})
+    .then((response) => {
+        console.log(
+            'The data given back by the API response is:',
+            response.data);
+        axios.get('http://127.0.0.1:5000/weather',
+            {params:{
+                lat: response.data[0].lat,
+                lon: response.data[0].lon
+            }}
+        )
+        .then((result) => {
+            const tempK = result.data.main.temp
+            const tempF = Math.round((tempK - 273.15) * 9/5 + 32);
+            console.log('Result #2: ', tempF)
+            changeColor(tempF)
+            temp.innerText = tempF;
+        })
+        .catch((error) => {
+            console.log(
+                'The error for #2 is:',
+                error.response.data
+            );
+        })
+    })
+    .catch((error) => {
+        console.log(
+            'The error given back by the API response is:',
+            error.response.data
+        );
+    });    
+};
 
 // Add event listeners to the buttons
-increaseTempControl.addEventListener('click', () => {
-  temperature += 1;
-  updateTemperature();
-});
 
-decreaseTempControl.addEventListener('click', () => {
-  temperature -= 1;
-  updateTemperature();
-});
+increaseTempControl.addEventListener('click', increaseTemperature);
+decreaseTempControl.addEventListener('click', decreaseTemperature);
+currentTempButton.addEventListener("click",getTemp);
 
-// Initialize the temperature display when the page loads
-updateTemperature();
+const defaultCity = "Seattle";
+document.getElementById("headerCityName").textContent = defaultCity;
